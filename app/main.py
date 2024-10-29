ff0d39c2ae27bf4d54c7f5e2fb7652ee21b4b740
Zoe Hendershot <157422109+zoehendershot@users.noreply.github.com>
2024-10-29 00:33:49 +0000

    added divide endpoint

--- app/main.py
+++ app/main.py
@@ -10,19 +10,16 @@ app = FastAPI()
 
 @app.get("/")  # zone apex
 def zone_apex():
-<<<<<<< HEAD
     return {"Hello": "Hello Zoe"}
-=======
-    return {"Hello": "Hello API"}
->>>>>>> 661468dda0051934e6ad7be7bd4eeb40d3c08169
 
 @app.get("/add/{a}/{b}")
 def add(a: int, b: int):
     return {"sum": a + b}
-<<<<<<< HEAD
 
 @app.get("/square/{a}")
 def square(a: int):
     return {"square": a ** 2}
-=======
->>>>>>> 661468dda0051934e6ad7be7bd4eeb40d3c08169
+
+@app.get("/divide/{a}/{b}")
+def divide(a: int, b: int):
+    return {"quotient": a / b}
