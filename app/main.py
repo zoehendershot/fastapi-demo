2917b8b831cada0e1f10a806a2a156f8bd3dbc9d
Zoe Hendershot <157422109+zoehendershot@users.noreply.github.com>
2024-12-05 14:34:17 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 
-from fastapi import FastAPI
-from typing import Optional
-from pydantic import BaseModel
-import json
-import os
+#from fastapi import FastAPI
+#from typing import Optional
+#from pydantic import BaseModel
+#import json
+#import os
 
-app = FastAPI()
+#app = FastAPI()
 
-@app.get("/")  # zone apex
-def zone_apex():
-    return {"Hello": "Hello Zoe"}
+#@app.get("/")  # zone apex
+#def zone_apex():
+    #return {"Hello": "Hello Zoe"}
 
 @app.get("/add/{a}/{b}")
 def add(a: int, b: int):
